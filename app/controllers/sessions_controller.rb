50e3d0526fb20b73bd550fccc6b687458c55275a
vickean <vickean@gmail.com>
2016-05-05 18:15:57 +0800

    small edits in controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < Clearance::SessionsController
 
   def create_from_omniauth
     auth_hash = request.env["omniauth.auth"]
-
+    #byebug
     authentication = Authentication.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"]) || Authentication.create_with_omniauth(auth_hash)
     if authentication.user
       user = authentication.user
